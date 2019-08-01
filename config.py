6a9602a10a6b04299d3b657a0dea9c8d408c2c23
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2019-08-01 16:07:32 +0200

    Solved issue with env. variables

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ MAX_TWEETS_PER_HOUR = 5
 if "APP_URL" in os.environ:
 	APP_URL = os.environ.get("APP_URL")
 if "TWITTER_ENV_NAME" in os.environ:
-	TWITTER_ENV_NAME = os.environ.get("APP_URL")
+	TWITTER_ENV_NAME = os.environ.get("TWITTER_ENV_NAME")
 if "MAX_TWEETS_PER_HOUR" in os.environ:
 	MAX_TWEETS_PER_HOUR = os.environ.get("MAX_TWEETS_PER_HOUR")
