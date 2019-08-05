a749f88586334873626b8873890b871c8e57be51
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2019-08-05 21:47:34 +0200

    Better looking welcome message

--- welcomemsg.py
+++ welcomemsg.py
@@ -1,4 +1,8 @@
 welcomemsgtext = """COTILLEOS FÍSICA UGR
-Para publicar un tweet anónimamente en esta cuenta envía el texto a publicar e interactúa con los botones que aparecerán en la pantalla. Para corregir el texto basta con enviarlo de nuevo. Para adjuntar archivos envíalos uno a uno adjuntos en un mensaje. Para citar un tweet envía el link de dicho tweet. En un mismo mensaje es posible realizar todas las acciones a la vez.
+	\u2022	Para publicar un tweet anónimamente en esta cuenta envía el texto a publicar e interactúa con los botones que aparecerán en la pantalla. 
+	\u2022	Para corregir el texto basta con enviarlo de nuevo. 
+	\u2022	Para adjuntar archivos envíalos uno a uno adjuntos en un mensaje. 
+	\u2022	Para citar un tweet envía el link de dicho tweet. 
+En un mismo mensaje es posible realizar todas las acciones a la vez.
 Se permiten un máximo de 5 tweets a la hora. Ya se envíen por separado o en un hilo.
 Un comportamiento no adecuado resultará en la imposibilidad de publicar más tweets"""
