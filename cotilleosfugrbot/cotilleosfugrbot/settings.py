7707cccdb54b3bed2725cfb2b7611338d6ebb45b
Jesus Cobos Jimenez <jesus.victoria5@gmail.com>
2020-07-03 19:53:07 +0200

    Added secret key from environ

--- cotilleosfugrbot/cotilleosfugrbot/settings.py
+++ cotilleosfugrbot/cotilleosfugrbot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from chatbot.config import APP_URL
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
+if "SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [APP_URL.split("/")[2]]
 
@@ -74,14 +77,19 @@ WSGI_APPLICATION = 'cotilleosfugrbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cotilleosfugrbot',
+        'USER': 'cotilleosfugrbot_server',
+        'PASSWORD': 'testpassword',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
