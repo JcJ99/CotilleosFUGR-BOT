a4e29c4f87b8c193d463551d6787e38e1a384131
Jesus Cobos Jimenez <jesus.victoria5@gmail.com>
2020-07-20 12:39:39 +0200

    Fixed heroku shceduler

--- cotilleosfugrbot/heroku_scheduler.py
+++ cotilleosfugrbot/heroku_scheduler.py
@@ -1,14 +1,16 @@
 from webhook_tools import put
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 import os
+import threading
 
-sched = BackgroundScheduler()
+sched = BlockingScheduler()
 
 PUT_INTERVAL = int(os.environ.get("PUT_INTERVAL", 10))
 
 @sched.scheduled_job('interval', minutes=PUT_INTERVAL)
 def timed_job():
-    put(print_log=False)
+    t = threading.Thread(target=put)
+    t.run()
     print("Llamada a twitter para mantener el servidor activo")
 
 sched.start()
